e5cba958bba5280ef398da2a9db73a4613325878
arhizmei <arhizmei@gmail.com>
2023-04-28 19:38:37 +0300

    work6

--- app/src/main/java/com/zmei/a3composelessons/MainActivity.kt
+++ app/src/main/java/com/zmei/a3composelessons/MainActivity.kt
@@ -1,12 +1,19 @@
 package com.zmei.a3composelessons
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.detectDragGesturesAfterLongPress
+import androidx.compose.foundation.gestures.detectHorizontalDragGestures
+import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
@@ -15,6 +22,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.compose.ui.res.painterResource
@@ -26,23 +34,38 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Column() {
-                listItem("Здесь может быть ваша рекалам", "Тут должно быть какое-то описание")
-                listItem("Здесь может быть ваша рекалам", "Тут должно быть какое-то описание")
-                listItem("Здесь может быть ваша рекалам", "Тут должно быть какое-то описание")
-                listItem("Здесь может быть ваша рекалам", "Тут должно быть какое-то описание")
-                listItem("Здесь может быть ваша рекалам", "Тут должно быть какое-то описание")
-                listItem("Здесь может быть ваша рекалам", "Тут должно быть какое-то описание")
+            Column(modifier = Modifier.verticalScroll(rememberScrollState())) {
+                listItem("Здесь может быть ваша реклама", "Тут должно быть какое-то описание")
+                listItem("Здесь может быть ваша реклама", "Тут должно быть какое-то описание")
+                listItem("Здесь может быть ваша реклама", "Тут должно быть какое-то описание")
+                listItem("Здесь может быть ваша реклама", "Тут должно быть какое-то описание")
+                listItem("Здесь может быть ваша реклама", "Тут должно быть какое-то описание")
+                listItem("Здесь может быть ваша реклама", "Тут должно быть какое-то описание")
+                listItem("Здесь может быть ваша реклама", "Тут должно быть какое-то описание")
+                listItem("Здесь может быть ваша реклама", "Тут должно быть какое-то описание")
+                listItem("Здесь может быть ваша реклама", "Тут должно быть какое-то описание")
+                listItem("Здесь может быть ваша реклама", "Тут должно быть какое-то описание")
+                listItem("Здесь может быть ваша реклама", "Тут должно быть какое-то описание")
+                listItem("Здесь может быть ваша реклама", "Тут должно быть какое-то описание")
+                listItem("Здесь может быть ваша реклама", "Тут должно быть какое-то описание")
+                listItem("Здесь может быть ваша реклама", "Тут должно быть какое-то описание")
+                listItem("Здесь может быть ваша реклама", "Тут должно быть какое-то описание")
+                listItem("Здесь может быть ваша реклама", "Тут должно быть какое-то описание")
 
             }
                 }
            }
 
 @Composable
-private fun listItem(name: String, prof: String){
+private fun listItem(reklama: String, opis: String){
 Card(modifier = Modifier
     .fillMaxWidth()
-    .padding(10.dp),
+    .padding(10.dp)
+    .pointerInput(Unit){
+        detectHorizontalDragGestures {
+                change, dragAmount ->
+            Log.d("myLog", "Long press $dragAmount")}},
+
     shape = RoundedCornerShape(15.dp),
     elevation = 5.dp
     ) {
@@ -58,8 +81,8 @@ Box() {
                 .clip(CircleShape)
             )
         Column(modifier = Modifier.padding(start = 15.dp)) {
-            Text(text = name)
-            Text(text = prof)
+            Text(text = reklama)
+            Text(text = opis)
 
         }
         
