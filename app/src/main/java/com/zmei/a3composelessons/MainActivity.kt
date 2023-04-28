cafdd728b01154437c976c520d85a0b06e22c691
arhizmei <arhizmei@gmail.com>
2023-04-28 13:37:55 +0300

    work 4

--- app/src/main/java/com/zmei/a3composelessons/MainActivity.kt
+++ app/src/main/java/com/zmei/a3composelessons/MainActivity.kt
@@ -3,43 +3,61 @@ package com.zmei.a3composelessons
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
-import com.zmei.a3composelessons.ui.theme
+import androidx.compose.ui.unit.dp
 
-.3ComposeLessonsTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            3 ComposeLessonsTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
+            listItem("Здесь может быть ваша рекалам", "Тут должно быть какое-то описание")
                 }
-            }
+           }
+
+@Composable
+private fun listItem(name: String, prof: String){
+Card(modifier = Modifier
+    .fillMaxWidth()
+    .padding(10.dp),
+    shape = RoundedCornerShape(15.dp),
+    elevation = 5.dp
+    ) {
+Box() { 
+    Row(verticalAlignment = Alignment.CenterVertically) {
+        Image(
+            painter = painterResource(id = R.drawable.primer),
+            contentDescription = "image",
+            contentScale = ContentScale.Crop,
+            modifier = Modifier
+                .padding(5.dp)
+                .size(65.dp)
+                .clip(CircleShape)
+            )
+        Column {
+            Text(text = name)
+            Text(text = prof)
+
         }
+        
     }
+    
+}
+}
+}
 }
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    3 ComposeLessonsTheme {
-        Greeting("Android")
-    }
-}
