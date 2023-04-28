fc1a475a8d75ff4fb45655cbdf04f7cc10442d27
arhizmei <arhizmei@gmail.com>
2023-04-28 13:52:42 +0300

    work5

--- app/src/main/java/com/zmei/a3composelessons/MainActivity.kt
+++ app/src/main/java/com/zmei/a3composelessons/MainActivity.kt
@@ -16,6 +16,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -25,7 +26,15 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            listItem("Здесь может быть ваша рекалам", "Тут должно быть какое-то описание")
+            Column() {
+                listItem("Здесь может быть ваша рекалам", "Тут должно быть какое-то описание")
+                listItem("Здесь может быть ваша рекалам", "Тут должно быть какое-то описание")
+                listItem("Здесь может быть ваша рекалам", "Тут должно быть какое-то описание")
+                listItem("Здесь может быть ваша рекалам", "Тут должно быть какое-то описание")
+                listItem("Здесь может быть ваша рекалам", "Тут должно быть какое-то описание")
+                listItem("Здесь может быть ваша рекалам", "Тут должно быть какое-то описание")
+
+            }
                 }
            }
 
@@ -48,7 +57,7 @@ Box() {
                 .size(65.dp)
                 .clip(CircleShape)
             )
-        Column {
+        Column(modifier = Modifier.padding(start = 15.dp)) {
             Text(text = name)
             Text(text = prof)
 
